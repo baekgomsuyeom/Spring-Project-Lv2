0fafa0ae0525761b70c7ddd357068f3847f3bfdc
baekgomsuyeom <jungin598@gmail.com>
2023-07-04 07:53:39 +0900

    ResponseEntity 적용

--- src/main/java/com/springproject/springprojectlv2/controller/BoardController.java
+++ src/main/java/com/springproject/springprojectlv2/controller/BoardController.java
@@ -6,6 +6,7 @@ import com.springproject.springprojectlv2.dto.MsgResponseDto;
 import com.springproject.springprojectlv2.service.BoardService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,31 +19,31 @@ public class BoardController {
 
     // 게시글 작성
     @PostMapping("/board")
-    public BoardResponseDto createBoard(@RequestBody BoardRequestDto requestDto, HttpServletRequest request) {
-        return boardService.createBoard(requestDto, request);
+    public ResponseEntity<BoardResponseDto> createBoard(@RequestBody BoardRequestDto requestDto, HttpServletRequest request) {
+        return ResponseEntity.ok(boardService.createBoard(requestDto, request));
     }
 
     // 게시글 전체 조회
     @GetMapping("/board")
-    public List<BoardResponseDto> getBoardList() {
-        return boardService.getBoardList();
+    public ResponseEntity<List<BoardResponseDto>> getBoardList() {
+        return ResponseEntity.ok(boardService.getBoardList());
     }
 
     // 게시글 선택 조회
     @GetMapping("/board/{id}")
-    public BoardResponseDto getBoard(@PathVariable Long id) {
-        return boardService.getBoard(id);
+    public ResponseEntity<BoardResponseDto> getBoard(@PathVariable Long id) {
+        return ResponseEntity.ok(boardService.getBoard(id));
     }
 
     // 게시글 수정
     @PutMapping("/board/{id}")
-    public BoardResponseDto updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto, HttpServletRequest request) {
-        return boardService.updateBoard(id, requestDto, request);
+    public ResponseEntity<BoardResponseDto> updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto, HttpServletRequest request) {
+        return ResponseEntity.ok(boardService.updateBoard(id, requestDto, request));
     }
 
     // 게시글 삭제
     @DeleteMapping("/board/{id}")
-    public MsgResponseDto deleteBoard(@PathVariable Long id, HttpServletRequest request) {
-        return boardService.deleteBoard(id, request);
+    public ResponseEntity<MsgResponseDto> deleteBoard(@PathVariable Long id, HttpServletRequest request) {
+        return ResponseEntity.ok(boardService.deleteBoard(id, request));
     }
 }
